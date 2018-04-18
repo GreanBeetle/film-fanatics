f866d595abf620b08811a8bd5fba4d3a208eb5e3
Bryce Frazier and John Rykken <bryce@>
2018-04-18 14:25:47 -0700

    Add edit film page. Problem-solve error.

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,8 +1,23 @@
 class FilmsController < ApplicationController
 
   def show
-    @year= Year.find(params[:year_id])
+    @year = Year.find(params[:year_id])
     @film = Film.find(params[:id])
     render :show
   end
+
+  def edit
+    @year = Year.find(params[:year_id])
+    @film = Film.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @film = Film.find(params[:id])
+  end
+
+  private
+    def films_params
+      params.require(:film).permit(:title, :director, :year_id, :genre, :opening_gross)
+    end
 end
