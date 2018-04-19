98d86d73beb1253a2477542d390a3493646a3f2f
Bryce Frazier and John Rykken <bryce@>
2018-04-19 09:40:43 -0700

    Add alphabetized_films scope to year display.

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,11 +1,13 @@
 class YearsController < ApplicationController
   def index
     @years = Year.all
+
     render :index
   end
 
   def show
     @year= Year.find(params[:id])
+    @alphabetized_films = Film.alphabetized_films
     render :show
   end
 
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,6 @@
 class Film < ActiveRecord::Base
   belongs_to :year
   validates :title, :presence => true
+
+  scope :alphabetized_films, -> {order(title: :asc)}
 end
